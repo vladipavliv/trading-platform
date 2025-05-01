935ea9a87c20542948a7242fe7caf37ca6f15b88
Vladimir Pavliv <voldodimir@gmail.com>
2025-05-01 05:20:43 +0200

    Minor renaming and cleanup

--- common/src/ctx_runner.hpp
+++ common/src/ctx_runner.hpp
@@ -3,8 +3,8 @@
  * @date 2025-02-13
  */
 
-#ifndef HFT_COMMON_WORKER_HPP
-#define HFT_COMMON_WORKER_HPP
+#ifndef HFT_COMMON_CTXRUNNER_HPP
+#define HFT_COMMON_CTXRUNNER_HPP
 
 #include <memory>
 
@@ -18,24 +18,24 @@ namespace hft {
 /**
  * @brief Worker thread with an io_context
  */
-class Worker {
+class CtxRunner {
 public:
-  using UPtr = std::unique_ptr<Worker>;
+  using UPtr = std::unique_ptr<CtxRunner>;
 
   IoCtx ioCtx;
 
-  Worker(ThreadId id, bool pinToCore, CoreId coreId = 0)
+  CtxRunner(ThreadId id, bool pinToCore, CoreId coreId = 0)
       : threadId_{id}, pinToCore_{pinToCore}, coreId_{coreId},
         guard_{MakeGuard(ioCtx.get_executor())} {}
 
-  ~Worker() { ioCtx.stop(); }
+  ~CtxRunner() { ioCtx.stop(); }
 
-  void start() {
-    if (started_) {
+  void run() {
+    if (running_) {
       LOG_ERROR("Worker is already running");
       return;
     }
-    started_ = true;
+    running_ = true;
     thread_ = Thread{[this]() {
       try {
         utils::setTheadRealTime();
@@ -46,7 +46,7 @@ public:
           LOG_INFO_SYSTEM("Starting worker thread {}", threadId_);
         }
         ioCtx.run();
-      } catch (const std::exception &e) {
+      } catch (CRef<std::exception> e) {
         LOG_ERROR_SYSTEM("Exception in worker thread {} {}", threadId_, e.what());
       }
     }};
@@ -59,7 +59,7 @@ private:
   const bool pinToCore_;
   const CoreId coreId_;
 
-  bool started_{false};
+  bool running_{false};
 
   ContextGuard guard_;
   Thread thread_;
@@ -67,4 +67,4 @@ private:
 
 } // namespace hft
 
-#endif // HFT_COMMON_WORKER_HPP
+#endif // HFT_COMMON_CTXRUNNER_HPP
